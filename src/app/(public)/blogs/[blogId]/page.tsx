7ae101b4fa6a01e5920c72f126b28c7cdcd805e8
imran-khan-dev <emrankhanniloy@gmail.com>
2025-10-10 00:19:46 +0600

    Add: dynamic blog + ISR + staticParams

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,13 +1,30 @@
 import BlogDetailsCard from "@/components/modules/Blogs/BlogDetailsCard";
+import { BlogPost } from "@/types";
 
-const BlogDetailsPage = async ({
-  params,
-}: {
-  params: Promise<{ blogId: string }>;
-}) => {
-  const { blogId } = await params;
+export const generateStaticParams = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/blogs`, {
+    next: { revalidate: 60 },
+  });
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/blog/${blogId}`);
+  const result = await res.json();
+  const blogs: BlogPost[] = result?.data?.data || [];
+
+  return blogs.slice(0, 3).map((blog) => ({
+    blogId: String(blog.id),
+  }));
+};
+
+const BlogDetailsPage = async ({ params }: { params: { blogId: string } }) => {
+  const { blogId } = params;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API}/blog/${blogId}`,
+    {
+      next: {
+        revalidate: 30,
+      },
+    }
+  );
 
   const { data: blog } = await res.json();
 
