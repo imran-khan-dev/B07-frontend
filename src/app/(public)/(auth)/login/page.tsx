2d475b50c8294f7afbff9f3202504b8879e9f11d
imran-khan-dev <emrankhanniloy@gmail.com>
2025-10-13 23:45:24 +0600

    Add: login form

---
+++ src/app/(public)/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import AdminLoginForm from "@/components/modules/Auth/AdminLogin";
+
+export default function AdminLoginPage() {
+  return (
+    <div>
+      <AdminLoginForm />
+    </div>
+  );
+}
