3ddf06d1f4af95d72ebdc9e67e547268dc13a8a6
imran-khan-dev <emrankhanniloy@gmail.com>
2025-10-16 19:23:53 +0600

    Add: dashboard updated

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -6,14 +6,6 @@ export async function SiteHeader() {
   const userRes = await getUser();
   const user = userRes?.data;
 
-  if (!user) {
-    return (
-      <div className="flex items-center justify-center h-screen text-lg text-gray-600">
-        Please login first.
-      </div>
-    );
-  }
-
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
